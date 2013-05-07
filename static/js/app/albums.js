ef88f3a5af4534cfffcf716ee5dc4e86c5636ea4
fabin <os.dev.bin@gmail.com>
2013-05-07 11:55:20 +0800

    add the album site cover

---
+++ static/js/app/albums.js
@@ -0,0 +1,10 @@
+/*
+ * IMPORTANT!!!
+ * REMEMBER TO ADD  rel="external"  to your anchor tags. 
+ * If you don't this will mess with how jQuery Mobile works
+ */
+$(document).ready(function() {
+
+	$('#cover .profile .details .head img').corner('60px');
+
+});
