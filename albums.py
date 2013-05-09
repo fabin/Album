68e298ddbdeb7bdf202eb33b138f5e286124f867
fabin <os.dev.bin@gmail.com>
2013-05-09 10:54:43 +0800

    validate creating album page

--- albums.py
+++ albums.py
@@ -14,24 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import jinja2
-import os
+from __future__ import with_statement
+from google.appengine.api import files, images, users
+from google.appengine.ext import blobstore, db
 from google.appengine.ext.webapp import blobstore_handlers
-from google.appengine.ext import blobstore
+import jinja2
+import json
 import logging
+import os
+import re
+import urllib
+import webapp2
+
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
     
-import webapp2
 
-from google.appengine.ext import db
-from google.appengine.api import users
 
 EXPIRATION_TIME = 300  # seconds
 
 class Album(db.Model):
     name = db.StringProperty()
+    cover = db.StringProperty()
     description = db.StringProperty(multiline=True)
     date = db.DateTimeProperty(auto_now_add=True)
 
@@ -80,6 +85,7 @@ class AlbumCreate(webapp2.RequestHandler):
         # should be limited to ~1/second.
         greeting = Album(parent=album_key());
         greeting.name = self.request.get('name')
+        greeting.cover = self.request.get('cover')
         greeting.description = self.request.get('description')
         greeting.put()
         self.redirect('/albums')
@@ -114,11 +120,133 @@ class AlbumPicture(blobstore_handlers.BlobstoreDownloadHandler):
             self.response.headers['Cache-Control'] = \
                 'public,max-age=%d' % EXPIRATION_TIME
             self.send_blob(picture.image)
+
+
+MIN_FILE_SIZE = 1  # bytes
+MAX_FILE_SIZE = 5000000  # bytes
+IMAGE_TYPES = re.compile('image/(gif|p?jpeg|(x-)?png)')
+ACCEPT_FILE_TYPES = IMAGE_TYPES
+
+class AlbumCoverUploadHandler(webapp2.RequestHandler):
+
+
+    def initialize(self, request, response):
+        super(AlbumCoverUploadHandler, self).initialize(request, response)
+        logging.info("initialize(...)")
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.headers[
+            'Access-Control-Allow-Methods'
+        ] = 'OPTIONS, HEAD, GET, POST, PUT, DELETE'
+        self.response.headers[
+            'Access-Control-Allow-Headers'
+        ] = 'Content-Type, Content-Range, Content-Disposition'
+
+    def validate(self, fileObject):
+        logging.info("validate(...)")
+        if fileObject['size'] < MIN_FILE_SIZE:
+            fileObject['error'] = 'File is too small'
+        elif fileObject['size'] > MAX_FILE_SIZE:
+            fileObject['error'] = 'File is too big'
+        elif not ACCEPT_FILE_TYPES.match(fileObject['type']):
+            fileObject['error'] = 'File type not allowed'
+        else:
+            return True
+        return False
+
+    def get_file_size(self, fileObject):
+        logging.info("get_file_size(...)")
+        fileObject.seek(0, 2)  # Seek to the end of the fileObject
+        size = fileObject.tell()  # Get the position of EOF
+        fileObject.seek(0)  # Reset the fileObject position to the beginning
+        return size
+
+    def write_blob(self, data, info):
+        logging.info("write_blob(...)")
+        blob = files.blobstore.create(
+            mime_type=info['type'],
+            _blobinfo_uploaded_filename=info['name']
+        )
+        with files.open(blob, 'a') as f:
+            f.write(data)
+        files.finalize(blob)
+        return files.blobstore.get_blob_key(blob)
+
+    def handle_upload(self):
+        logging.info("handle_upload(...)")
+        results = []
+        blob_keys = []
+        for name, fieldStorage in self.request.POST.items():
+            if type(fieldStorage) is unicode:
+                continue
+            result = {}
+            result['name'] = re.sub(
+                r'^.*\\',
+                '',
+                fieldStorage.filename
+            )
+            result['type'] = fieldStorage.type
+            result['size'] = self.get_file_size(fieldStorage.file)
+            if self.validate(result):
+                blob_key = str(
+                    self.write_blob(fieldStorage.value, result)
+                )
+                blob_keys.append(blob_key)
+                
+#                 album_key = self.request.get('album_key')
+                image_url = images.get_serving_url(
+                            blob_key,
+                            secure_url=self.request.host_url.startswith(
+                                'https'
+                            )
+                        )
+                result['url'] = image_url
+                logging.info("url == " + result['url'])
+
+                result['delete_type'] = 'DELETE'
+                result['delete_url'] = self.request.host_url + \
+                    '/?key=' + urllib.quote(blob_key, '')
+                    
+#                 if (IMAGE_TYPES.match(result['type'])):
+#                     try:
+#                         result['url'] = images.get_serving_url(
+#                             blob_key,
+#                             secure_url=self.request.host_url.startswith(
+#                                 'https'
+#                             )
+#                         )
+#                         result['thumbnail_url'] = result['url'] + \
+#                             THUMBNAIL_MODIFICATOR
+#                     except:  # Could not get an image serving url
+#                         pass
+#                 if not 'url' in result:
+#                     result['url'] = self.request.host_url + \
+#                         '/' + blob_key + '/' + urllib.quote(
+#                             result['name'].encode('utf-8'), '')
+#                     result['thumbnail_url'] = result['url'] + THUMBNAIL_MODIFICATOR
+            results.append(result)
+#         deferred.defer(
+#             cleanup,
+#             blob_keys,
+#             _countdown=EXPIRATION_TIME
+#         )
+        return results
+
+    def post(self):
+        logging.info("post(...)")
         
+        if (self.request.get('_method') == 'DELETE'):
+            return self.delete()
+        result = {'files': self.handle_upload()}
+        s = json.dumps(result, separators=(',', ':'))
+        if 'application/json' in self.request.headers.get('Accept'):
+            self.response.headers['Content-Type'] = 'application/json'
+        self.response.write(s)
+
 app = webapp2.WSGIApplication([('/albums', Albums),
                                ('', Albums),
                                ('/', Albums),
                                ('/albums/picture/([^/]+)', AlbumPicture),
                                ('/albums/details/([^/]+)', AlbumDetails),
+                               ('/albums/create/cover', AlbumCoverUploadHandler),
                                ('/albums/create', AlbumCreate)],
                               debug=True)
