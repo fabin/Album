2e72cf11a0f1f158017f9f308ef9c5fba0ba28ae
TonnyTao <tonny.litao@gmail.com>
2013-05-28 16:20:41 +0800

    Add Left-Drawer logic

---
+++ ios/WeddingAlbum/WeddingAlbum/Class/ViewControllers/WAMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  WAMenuViewController.h
+//  WeddingAlbum
+//
+//  Created by Tonny on 5/28/13.
+//  Copyright (c) 2013 SlowsLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAMenuViewController : UITableViewController
+
+@end
---
+++ ios/WeddingAlbum/WeddingAlbum/Class/ViewControllers/WAMenuViewController.m
@@ -0,0 +1,70 @@
+//
+//  WAMenuViewController.m
+//  WeddingAlbum
+//
+//  Created by Tonny on 5/28/13.
+//  Copyright (c) 2013 SlowsLab. All rights reserved.
+//
+
+#import "WAMenuViewController.h"
+
+@interface WAMenuViewController ()
+
+@end
+
+@implementation WAMenuViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+@end
