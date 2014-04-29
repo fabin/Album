bd669e58ad30c1600ed940bfd4c1e79076568661
fabin <os.dev.bin@gmail.com>
2014-04-29 16:25:28 +0800

    fix album image layout.

--- appengine/static/js/app/album.details-image.js
+++ appengine/static/js/app/album.details-image.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#freewall .cell').each(function(index, value) {
-		var w = 200 + 200 * Math.random() << 0;
+		var w = 300 + 200 * Math.random() << 0;
 		$(value).width(w);
 		$(value).height(200);
 	});
@@ -10,7 +10,7 @@ $(document).ready(function() {
 		selector : '.cell',
 		animate : true,
 		cellW : 20,
-		cellH : 200,
+		cellH : 300,
 		onResize : function() {
 			wall.fitWidth();
 		}
